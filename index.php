82a52bd658340edada6d957e401f10e0f452ff01
Wattanachai Maijampa <wmaijampa549@gmail.com>
2024-07-27 22:09:40 +0700

    testinng

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	
+</head>
+<style type="text/css">
+	#cen{text-align:center;}
+	.border{border: 5px black solid;}
+	#red{margin: 0px;}
+</style>
+<body id="red">
+<div id="cen">
+	<form class="border">
+		<label id="username">username:</label>
+		<input type="text" name="" id="username"><br>
+		<label id="password">password:</label>
+		<input type="text" name="" id="password"><br>
+		<input type="submit">
+	</form>
+</div>
+
+
+</body>
+</html>
